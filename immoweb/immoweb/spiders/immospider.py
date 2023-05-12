dcb1ce03eb09de9ca6c3219138e2355fcdb8fe32
Savchuk Volodymyr <131377510+svstm@users.noreply.github.com>
2023-05-12 16:02:12 +0200

    all url from 1 page

--- immoweb/immoweb/spiders/immospider.py
+++ immoweb/immoweb/spiders/immospider.py
@@ -1,28 +1,18 @@
-"""import scrapy
 
-
-class ImmospiderSpider(scrapy.Spider):
-    name = "immospider"
-    allowed_domains = ["www.immoweb.be"]
-    start_urls = ["https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE"]
-
-    def parse(self, response):
-        pass
-        """
 import scrapy
 
 class ImmospiderSpider(scrapy.Spider):
     name = "immospider"
-    # allowed_domains = ["www.immoweb.be"]
-    # start_urls = ["https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE"]
-     #property_url = "https://www.immoweb.be/en/classified/apartment/for-sale/deinze/9800/10560242"
-
     def start_requests(self):
        yield scrapy.Request("https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE", 
                             meta={'playwright' : True})
 
 
     def parse(self, response):
-        yield{
-            'text' : response.text
-        }
+       
+        properties = response.css('article.card.card--result.card--xl')
+
+        for property in properties:
+            yield {
+                    'url': property.css('h2 a').attrib['href']
+            }
