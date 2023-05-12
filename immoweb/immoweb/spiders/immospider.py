2576f185809c1ad3f576ca5f64390273eda55b90
Savchuk Volodymyr <131377510+svstm@users.noreply.github.com>
2023-05-12 14:10:07 +0200

    Recive json1

--- immoweb/immoweb/spiders/immospider.py
+++ immoweb/immoweb/spiders/immospider.py
@@ -1,4 +1,4 @@
-import scrapy
+"""import scrapy
 
 
 class ImmospiderSpider(scrapy.Spider):
@@ -8,3 +8,21 @@ class ImmospiderSpider(scrapy.Spider):
 
     def parse(self, response):
         pass
+        """
+import scrapy
+
+class ImmospiderSpider(scrapy.Spider):
+    name = "immospider"
+    # allowed_domains = ["www.immoweb.be"]
+    # start_urls = ["https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE"]
+     #property_url = "https://www.immoweb.be/en/classified/apartment/for-sale/deinze/9800/10560242"
+
+    def start_requests(self):
+       yield scrapy.Request("https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE", 
+                            meta={'playwright' : True})
+
+
+    def parse(self, response):
+        yield{
+            'text' : response.text
+        }
