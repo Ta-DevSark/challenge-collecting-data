fa75e7423a77104284691c4b28046e2e104f32de
ChristosRaptis <chrisgraptis@gmail.com>
2023-05-22 08:11:46 +0200

    Finaly done, got the urls and data of all 333 pages in search

--- immoweb/immoweb/items.py
+++ immoweb/immoweb/items.py
@@ -8,5 +8,22 @@ import scrapy
 
 class ImmowebItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    locality = scrapy.Field()
+    property_type = scrapy.Field()
+    subtype_property = scrapy.Field()
+    price = scrapy.Field()
+    type_of_sale = scrapy.Field()
+    number_of_rooms = scrapy.Field()
+    living_area = scrapy.Field()
+    kitchen_fully_equiped = scrapy.Field()
+    is_furnished = scrapy.Field()
+    has_open_fire = scrapy.Field()
+    has_terrace = scrapy.Field()
+    terrace_area = scrapy.Field()
+    has_garden = scrapy.Field()
+    garden_surface = scrapy.Field()
+    habitable_surface = scrapy.Field()
+    plot_land_surface = scrapy.Field()
+    number_of_facades = scrapy.Field()
+    has_swimming_pool = scrapy.Field()
+    building_state = scrapy.Field() 
